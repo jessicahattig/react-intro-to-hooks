1ab70b53f700061a680aeaa8de25225fb3dd1ea9
jessicahattig <jessicahattig@gmail.com>
2024-01-28 11:35:29 -0800

    Update Counter.js, App.js and add Timer.js

---
+++ src/Timer.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer(timerState => timerState + 1)
+      }, 1000
+    )}
+    //This is oneway to rewrite the arrow function as above.
+    //setTimer(timerState => {
+  //return timerState + 1
+//})
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  return (
+    <React.Fragment>
+      {isActive ? <h1>{timer}</h1> : <h1>Timer Stopped</h1>}
+      <button onClick={() => setIsActive(!isActive)}>Start/Stop</button>
+    </React.Fragment>
+  );
+}
+
+export default Timer;
